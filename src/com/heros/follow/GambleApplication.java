24c13b74eb42f0647526ec7fa6d0562eeaa80978
albert <albert>
2017-02-06 19:59:26 +0800

    command pattern 未完1

--- src/com/heros/follow/GambleApplication.java
+++ src/com/heros/follow/GambleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GambleApplication {
+	//123
 	public static void main(String[] args) {
 		SpringApplication.run(GambleApplication.class, args);
 	}
